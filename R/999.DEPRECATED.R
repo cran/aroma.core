d80201ae531b247cc9f0d62c101c963df7fd3522
Henrik Bengtsson <henrikb@braju.com>
2013-04-25 07:17:25 +0000

    version 2.9.0

--- R/999.DEPRECATED.R
+++ R/999.DEPRECATED.R
@@ -1,19 +1,20 @@
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 2009-06-09
 # o Grammar fix: is(Homo|Hetero)zygous(), not is(Homo|Hetero)zygote().
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 setMethodS3("isHomozygote", "AromaUnitGenotypeCallFile", function(...) {
-  .Deprecated("isHomozygous");
+  .Defunct("isHomozygous");
   isHomozygous(...);
 }, private=TRUE, deprecated=TRUE)
 
 setMethodS3("isHeterozygote", "AromaUnitGenotypeCallFile", function(...) {
-  .Deprecated("isHeterozygous");
+  .Defunct("isHeterozygous");
   isHeterozygous(...);
 }, private=TRUE, deprecated=TRUE)
 
+
 setMethodS3("getPhysicalPositions", "RawCopyNumbers", function(this, ...) {
-  .Deprecated("getPositions");
+  .Defunct("getPositions");
   getPositions(this, ...);
 }, protected=TRUE, deprecated=TRUE)
 
@@ -24,7 +25,7 @@ setMethodS3("readData", "SampleAnnotationFile", function(this, ...) {
   .Deprecated("readDataFrame");
   readDataFrame(this, ...);
 }, protected=TRUE, deprecated=TRUE)
- 
+
 
 
 # 2009-12-30
@@ -35,8 +36,25 @@ setMethodS3("nbrOfArrays", "AromaMicroarrayDataSetTuple", function(this, ...) {
 }, protected=TRUE)
 
 
+# 2013-01-03
+# o Deprecated nbrOfArrays() for AromaMicroarrayDataSet and
+#   AromaUnitTotalCnBinarySet.
+setMethodS3("nbrOfArrays", "AromaMicroarrayDataSet", function(this, ...) {
+  .Deprecated("length");
+  length(this, ...);
+})
+
+setMethodS3("nbrOfArrays", "AromaUnitTotalCnBinarySet", function(this, ...) {
+  .Deprecated("length");
+  length(this, ...);
+}, protected=TRUE)
+
+
+
 ############################################################################
 # HISTORY:
+# 2013-04-20
+# o CLEANUP: Made is(Homo|Hetero)zygote() defunct.
 # 2012-10-14
 # o Created 999.DEPRECATED.R.
 # 2011-02-18
