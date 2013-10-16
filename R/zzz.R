79504a9b05af251eea73a12b9ec0beac2cbd18bf
Henrik Bengtsson <henrikb@braju.com>
2013-10-16 00:00:00 +0000

    version 2.11.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,24 +1,12 @@
-# Allows conflicts. For more information, see library() and
-# conflicts() in [R] base.
-# .conflicts.OK <- TRUE
-
-# WORKAROUND: In order for the package to work with the most recent
-# version of R devel, which automatically add namespaces to packages
-# who do not have one, we explicitly have specify the following.
-# /HB 2011-07-27
-##cat <- R.utils::cat;
-##getOption <- R.utils::getOption;
-##lapply <- R.utils::lapply;
-append <- R.filesets::append;
-##sapply <- R.filesets::sapply;
-
-##.First.lib <- function(libname, pkgname) {
-.onAttach <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) {
+  ns <- getNamespace(pkgname);
   pkg <- AromaCore(pkgname);
-  assign(pkgname, pkg, pos=getPosition(pkg));
+  assign(pkgname, pkg, envir=ns);
+} # .onLoad()
 
-  # Setup package
+
+.onAttach <- function(libname, pkgname) {
+  pkg <- get(pkgname, envir=getNamespace(pkgname));
   .setupAromaCore(pkg);
-
   startupMessage(pkg);
 }
